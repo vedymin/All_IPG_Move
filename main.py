325bdd92df539bb2bb5c9133baab982077205913
vedymin <39482545+vedymin@users.noreply.github.com>
2018-05-29 18:14:00 +0200

    error handling for wrong hd

--- main.py
+++ main.py
@@ -3,58 +3,77 @@ import main_gui
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 
-
 conn = ConnectionManager()
+global hd_from
+global hd_to
 
 
 def add_connections():
-
     ui.choose_session_combo.clear()
     connections = conn.get_available_connections()
     ui.choose_session_combo.addItems(connections)
 
-def check_loggged():
 
+def check_loggged():
     session = ui.choose_session_combo.currentText()
     conn.open_session(session)
 
     if conn.check_logged_in(session):
         print("logged in")
     else:
-        error_non_logged()
+        error_msg("You are not logged in here. Log in or choose another session")
         print("not logged in")
 
 
 def start_moving():
+    global hd_from
+    global hd_to
 
     hd_from = ui.from_line_edit.text()
     hd_to = ui.to_line_edit.text()
 
-
-
     check_loggged()
+    check_hd()
 
 
+def check_hd():
+    global hd_from
+    global hd_to
 
-def error_non_logged():
+    if not hd_from.isdigit() or hd_to.isdigit():
+        error_msg("One of HD have letters inside.")
+        return
+
+    if len(hd_from) < 18 or len(hd_to) < 18:
+        error_msg("One of the HD is too short.")
+        return
+    elif len(hd_from) > 18:
+        hd_from = hd_from[-18:len(hd_from)]
+        ui.from_line_edit.setText(hd_from)
+    elif len(hd_to) > 18:
+        hd_to = hd_to[-18:len(hd_to)]
+        ui.to_line_edit.setText(hd_to)
+
+
+def error_msg(info):
     msg = QMessageBox()
     msg.setIcon(QMessageBox.Warning)
-    msg.setText("You are not logged in here. Log in or choose another session")
+    msg.setText(info)
     msg.setWindowTitle("Warning")
     msg.setStandardButtons(QMessageBox.Ok)
     msg.show()
     msg.exec_()
 
 
-
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = main_gui.Ui_MainWindow()
     ui.setupUi(MainWindow)
     ui.refresh_session_button.clicked.connect(add_connections)
-    ui.start_button.clicked.connect(check_loggged)
+    ui.start_button.clicked.connect(start_moving)
     add_connections()
     MainWindow.show()
     sys.exit(app.exec_())
